ff9534bee5bc6a543b543f1f85a1941f95d47ed9
Olatunbosun Ibiyinka <olatunbosunkayode47@gmail.com>
2025-09-20 17:54:40 +0100

    Remote backend created and Terraform code for ACR and AKS

---
+++ main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.99"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+module "acr" {
+  source              = "../../modules/acr"
+  acr_name                = var.acr_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+module "aks" {
+  source              = "../../modules/aks"
+  aks_name                = var.aks_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  acr_id              = module.acr.acr_id
+}
+

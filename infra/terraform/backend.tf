c6767f33aa7a6b550e8085fda5af59c8254b1f26
Olatunbosun Ibiyinka <olatunbosunkayode47@gmail.com>
2026-06-10 21:21:47 +0100

    fix(terraform): force OIDC-only auth for remote state backend init

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -6,6 +6,7 @@ terraform {
     key                  = "terraform.tfstate"
     use_azuread_auth     = true
     use_oidc             = true
+    subscription_id      = "cd258f56-ee0f-45e2-976a-a78ae7e93d8c"
     tenant_id            = "5ee10fdc-b731-4ee9-9181-0dad7378a345" # Your tenant ID
   }
 }

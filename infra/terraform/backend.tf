6b6881f83cef3e27de5d00ee266cafbc13d24b53
Olatunbosun Ibiyinka <olatunbosunkayode47@gmail.com>
2026-06-10 21:07:50 +0100

    fix(ci): OIDC auth for Terraform backend and non-blocking SonarCloud

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -5,6 +5,7 @@ terraform {
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
     use_azuread_auth     = true
+    use_oidc             = true
     tenant_id            = "5ee10fdc-b731-4ee9-9181-0dad7378a345" # Your tenant ID
   }
 }
